bbd42840c0f9525bc873420c8a1f66490734bca3
Subhan Ali <subhan@1001dubai.com>
2024-04-15 16:42:46 +0500

    Basic func implemented to randomly pick another image from assets whenever a user clicks on the image.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,53 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  const BallPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          backgroundColor: Colors.blue[900],
+          centerTitle: false,
+          title: Text(
+            "Ask Me Anything",
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        body: Ball());
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({super.key});
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {
+        setState(() {
+          ballNumber = Random().nextInt(5) + 1;
+        });
+      },
+      child: Center(
+        child: Image.asset('images/ball$ballNumber.png'),
+      ),
+    );
+  }
+}
